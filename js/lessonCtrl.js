d73481a3b5b82023c5ed4cea1c4237f60eefd399
ascotson <ascotson@gmail.com>
2016-01-28 21:50:40 -0700

    All steps completed & successfully completed except Step 8.2 & 8.3.

--- js/lessonCtrl.js
+++ js/lessonCtrl.js
@@ -4,6 +4,12 @@ app.controller('lessonCtrl', function($scope) {
 
   $scope.lessons = ['Services', 'Routing', 'Directives', 'Review', 'Firebase', 'No server project', 'Node', 'Express', 'Mongo'];
 
-  $scope.test = 'Two way data binding!';
-
+  $scope.announceDay = function(lesson, day) {
+    if (day === undefined) {
+      alert('This lesson is not in the schedule.');
+    }
+    else {
+      alert(lesson + ' is active on ' + day + '.');
+    }
+  };
 });
--- js/lessonHiderDirective.js
+++ js/lessonHiderDirective.js
@@ -5,9 +5,24 @@ app.directive('lessonHider', function() {
     templateUrl: './js/lessonHider.html', //lessonHider.html position referenced from the index.html file since the template is being invoked from the directive elements within index.html.
     restrict: 'E',
     scope: {
-      lesson: '='
+      lesson: '=',
+      dayAlert: '&'
+    },
+    controller: function($scope, lessonService) {
+      $scope.getSchedule = lessonService.getSchedule();
     },
     link: function(scope, element, attrs) {
+      scope.getSchedule.then(function(response) {
+        scope.schedule = response.data;
+
+        scope.schedule.forEach(function(scheduleDay) {
+          if (scheduleDay.lesson === scope.lesson) {
+            element.css('text-decoration', 'line-through');
+            scope.lessonDay = schedule.weekday;
+            return;
+          }
+        });
+      });
     }
   };
 });
